24b5ea1194b50b4e764d59a16ec4423ca1d069df
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-10 05:53:20 -0700

    [sayali] Added:Loop through multiple times of flipping a coin

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -8,3 +8,22 @@ then
 else
 	echo "tail wins"
 fi
+headCount=0
+tailCount=0
+read -p "Enter the number of times you want to flip the coin:" n
+echo $n
+for (( i=0; i<$n; i++ ))
+do
+	flipCoin=$(($RANDOM%2+1))
+	if [ $flipCoin -eq 1 ]
+	then
+		echo "head wins"
+		headCount=$(($headCount+1))
+	else
+		echo "tailwins"
+		tailCount=$(($tailCount+1))
+	fi
+done
+echo "head wins:" $headCount "times"
+echo "tail wins:" $tailCount "times"
+
