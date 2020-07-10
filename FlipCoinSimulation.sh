d79c5e4ba66376b9133384476304f396cd82635b
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-10 05:43:36 -0700

    [sayali] Added:Simulator start with flipping coin

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo "Welcome to flip coin simulation program"
+flipCoin=$(($RANDOM % 2 + 1))
+echo $flipCoin
+if [ $flipCoin -eq 1 ]
+then
+	echo "head wins"
+else
+	echo "tail wins"
+fi
